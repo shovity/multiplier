09b0d6133099596c53912369102d060fa07a84f2
shovity <shovity@hub_development_1>
2021-07-05 03:33:51 +0000

    add api order

--- app.js
+++ app.js
@@ -8,31 +8,52 @@ const app = express()
 const server = http.createServer(app)
 
 app.use('/static', express.static('static'))
+app.use(express.json())
 
 
 if (config.master) {
     const io = require('./io')
     io.attach(server)
 
-    app.post('/order', (req, res, next) => {
-
-        
-    })
-
-    const data = [3, 6]
-
-    const done = (result) => {
-        console.log('done', result)
-    }
-
-    const handle = (data) => {
-        return data.map(a => a**a)
-    }
-
-    setTimeout(() => {
+    const work = (inputs, handle, done) => {
         const sockets = [...io.of("/").sockets].map(c => c[1])
-        sockets[0].emit('order', handle.toString(), data, done)
-    }, 500)
+        const freeSockets = sockets.filter(s => !s.busy)
+
+        const splitLevel = Math.min(
+            Math.ceil(inputs.length / freeSockets.length),
+            inputs.length,
+        )
+
+        const chucks = []
+            
+        for (let i = 0; i < inputs.length; i += splitLevel) {
+            chucks.push(inputs.slice(i, i + splitLevel))
+        }
+
+        const promises = chucks.map((c, index) => {
+            return new Promise((resolve, reject) => {
+                freeSockets[index].emit('order', handle.toString(), c, (result) => {
+                    resolve(result)
+                })
+            })
+        })
+
+        Promise.all(promises).then((resultSegments) => {
+            const results = resultSegments.reduce((pre, cur) => pre.concat(cur), [])
+            done(results)
+        })
+    }
+
+    app.post('/order', (req, res, next) => {
+        const handle = req.body.handle
+        const inputs = req.body.inputs
+
+        work(inputs, handle, (results) => {
+            res.json({
+                results,
+            })
+        })
+    })
 }
 
 server.listen(3000, () => {
